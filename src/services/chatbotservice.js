0043ceaf5d006e0c5db0052e6d097532ab1b04c6
wuyxz <wuyxz@wuyxzs-MacBook-Pro.local>
2021-06-27 21:54:44 +0700

    df

--- src/services/chatbotservice.js
+++ src/services/chatbotservice.js
@@ -4,8 +4,9 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 let handleGetStarted = (sender_psid) => {
   return new Promise(async (resolve, reject) => {
     try {
+      let username = await getUser(sender_psid);
       let response = {
-        text: `Chào cậu. Mình là Chill with wuyxz - một messenger chatbot. Chúc cậu ngày mới tốt lành <3`,
+        text: `Chào ${username}. Mình là Chill with wuyxz - một messenger chatbot. Chúc cậu ngày mới tốt lành <3`,
       };
       await callSendApi(sender_psid, response);
       resolve("Success");
@@ -13,6 +14,28 @@ let handleGetStarted = (sender_psid) => {
       reject(error);
     }
   });
+};
+
+let getUser = async (sender_psid) => {
+  // Send the HTTP request to the Messenger Platform
+  let username = "";
+  await request(
+    {
+      uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "GET",
+    },
+    (err, res, body) => {
+      console.log(body);
+      if (!err) {
+        let response = JSON.parse(res);
+        username = `${response.first_name} ${response.last_name}`;
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+  return username;
 };
 
 let callSendApi = (sender_psid, response) => {
