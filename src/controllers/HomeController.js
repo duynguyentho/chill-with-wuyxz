f31c5c18f95b2b17c8c902adcd60b88f1e783f38
wuyxz <wuyxz@wuyxzs-MacBook-Pro.local>
2021-06-27 12:58:34 +0700

    hid

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -118,6 +118,8 @@ function handlePostback(sender_psid, received_postback) {
     response = { text: "Thanks!" };
   } else if (payload === "no") {
     response = { text: "Oops, try sending another image." };
+  } else if (payload === "GET_STARTED") {
+    response = { text: `Chào cậu. Mình là Chill with wuyxz - một messenger chatbot. Chúc cậu ngày mới tốt lành <3` };
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
