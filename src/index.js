02c422f009fa247355be6ad483c317792d217078
wuyxz <wuyxz@wuyxzs-MacBook-Pro.local>
2021-06-27 11:29:07 +0700

    abc

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-'use strict';
+"use strict";
 import express from "express";
 import bodyParser from "body-parser";
-import viewEngine from "./config/viewEngine"
-import webRoutes from "./routes/web"
+import viewEngine from "./config/viewEngine";
+import webRoutes from "./routes/web";
 
 const app = express();
-const port = process.env.PORT || 8080;
-viewEngine(app);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+viewEngine(app);
 webRoutes(app);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
