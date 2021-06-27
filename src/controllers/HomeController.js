834fb8288037844e86e4c7acb6c710eb24ba8c46
wuyxz <wuyxz@wuyxzs-MacBook-Pro.local>
2021-06-27 11:32:27 +0700

    heroku

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -74,7 +74,10 @@ function handleMessage(sender_psid, received_message) {
 }
 
 // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
+function handlePostback(sender_psid, received_postback) {}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
   // Construct the message body
   let request_body = {
     recipient: {
@@ -99,17 +102,6 @@ function handlePostback(sender_psid, received_postback) {
       }
     }
   );
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-  // Construct the message body
-  let request_body = {
-    recipient: {
-      id: sender_psid,
-    },
-    message: response,
-  };
 }
 
 module.exports = {
